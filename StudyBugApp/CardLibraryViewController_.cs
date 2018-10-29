1801fbeb683e055e6f259ee96980390928982206
Nisini Dias <dias0023@algonquinlive.com>
2018-10-29 15:40:09 -0400

    Card Library, create card, and edit card pages designed. Needs to do connection.

---
+++ StudyBugApp/CardLibraryViewController_.cs
@@ -0,0 +1,18 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace StudyBugApp
+{
+    public partial class CardLibraryViewController : UIViewController
+    {
+        public CardLibraryViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        partial void UIButton35794_TouchUpInside(UIButton sender)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StudyBugApp/EditViewController.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace StudyBugApp
+{
+    public partial class EditViewController : UIViewController
+    {
+        public EditViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
