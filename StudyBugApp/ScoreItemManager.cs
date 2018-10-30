bf43ffb11aec1fd561c2808f5f10ec8c44899a87
Danny <ugawugga@hotmail.com>
2018-10-30 02:48:30 -0400

    Added classes for database management

---
+++ StudyBugApp/ScoreItemManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace StudyBugApp
+{
+    //accessoble methods to call throughout the app to modify the database- every method that needs to be accessible in database
+    //should be referenced here
+    public static class ScoreItemManager
+    {
+        static ScoreItemManager()
+        {
+        }
+        //returns the full item at an id value
+        public static ScoreItem GetItem(int id)
+        {
+            return ScoreItemRepositoryADO.GetItem(id);
+        }
+        //supposed to return all score items with intention of sticking them in some kind of list view
+        public static IList<ScoreItem> GetItems()
+        {
+            return new List<ScoreItem>(ScoreItemRepositoryADO.GetTasks());
+        }
+        //saves a score item (autoincrements primary key)
+        public static int SaveItem(ScoreItem item)
+        {
+            return ScoreItemRepositoryADO.SaveItem(item);
+        }
+        //deletes score item in the database at given ID
+        public static int DeleteItem(int id)
+        {
+            return ScoreItemRepositoryADO.DeleteItem(id);
+        }
+    }
+}
---
+++ StudyBugApp/ScoreItemRepositoryADO.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace StudyBugApp
+{
+    //repository design pattern implentation for database
+    public class ScoreItemRepositoryADO
+    {
+        ScoreDatabase db = null;
+        protected static string dbLocation;
+        public static ScoreItemRepositoryADO me;
+
+        static ScoreItemRepositoryADO()
+        {
+            me = new ScoreItemRepositoryADO();
+        }
+
+        public ScoreItemRepositoryADO()
+        {
+            // set file location and instatiate the database
+            dbLocation = DatabaseFilePath;
+            db = new ScoreDatabase(dbLocation);
+        }
+
+        public static string DatabaseFilePath
+        {
+            get
+            {
+                //Setting the location of the database
+                var sqliteFilename = "ScoreDatabase1.db3";
+                string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // Documents folder
+                string libraryPath = Path.Combine(documentsPath, "..", "Library"); // Library folder
+                var path = Path.Combine(libraryPath, sqliteFilename);
+                return path;
+            }
+        }
+
+        public static ScoreItem GetItem(int id)
+        {
+            return me.db.GetItem(id);
+        }
+
+        public static IEnumerable<ScoreItem> GetTasks()
+        {
+            return me.db.GetItems();
+        }
+
+        public static int SaveItem(ScoreItem item)
+        {
+            return me.db.SaveItem(item);
+        }
+
+        public static int DeleteItem(int id)
+        {
+            return me.db.DeleteItem(id);
+        }
+    }
+}
