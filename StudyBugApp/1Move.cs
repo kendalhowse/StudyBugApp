89aa8e6679acf991e047054303d24222ca1c0a53
Danny <ugawugga@hotmail.com>
2018-11-26 02:09:24 -0500

    re-added game/comments

--- StudyBugApp/1Move.cs
+++ StudyBugApp/1Move.cs
@@ -4,20 +4,22 @@ using Foundation;
 using GameplayKit;
 
 namespace StudyBugApp
-
+//This code is based on code from Xamarin sample project "four in a row" at https://developer.xamarin.com/samples/monotouch/ios9/FourInARow/
+//written/modified by Daniel Reilander
+//movement class for cube game
 {
     public class Move : NSObject, IGKGameModelUpdate
     {
 
         public nint Value { get; set; }
-
+    //get/set column number
         public int Column { get; private set; }
 
         Move(int column)
         {
             Column = column;
         }
-
+    //move method takes column parameter
         public static Move MoveInColumn(int column)
         {
             return new Move(column);
