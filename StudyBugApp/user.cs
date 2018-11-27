756b27255129ab804ffbc7f9eecf39ba3d648c90
Ahmed Mohammed <moha0701@algonquinlive.com>
2018-11-27 00:23:23 -0500

    Ahmed: add comments to User class

--- StudyBugApp/user.cs
+++ StudyBugApp/user.cs
@@ -9,20 +9,26 @@ using SQLite;
 //this class create map of objects to database
 namespace StudyBugApp
 {
+    /// <summary>
+    /// Author: Ahmed Mohammed
+    /// create user table
+    /// </summary>
+    /// 
+    //annotation to create table User in database
     [Table("User")]
     class User
     {
-
+        //set primary key and autoincrement in table in database
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
-
+        //create Auto-setter getter properties And Column name
         public string FirstName { get; set; }
-
+        //create Auto-setter getter properties And Column name
         public string LastName { get; set; }
-
+        //create Auto-setter getter properties And Column name set unique value constraints and max is 20 char
         [MaxLength(20), Unique]
         public string Email { get; set; }
-
+        //create Auto-setter getter properties And Column name set column max to 20 char for passowrd
         [MaxLength(20)]
         public string Password { get; set; }
         
